aa370b623d7859ecf0afa978d57024ff724bc2b6
PaschalO <paschal.oduoza@gmail.com>
2023-09-23 17:34:27 -0400

    Add shopping cart functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { ProductsModule } from "./features/pages/products/products.module";
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AuthenticationModule} from "./authentication/authentication.module";
+import {RouterOutlet} from "@angular/router";
+import { AppRoutingModule } from './app-routing.module';
+import { CartModule } from "./features/pages/cart/cart.module";
+import { SharedModule } from "./shared/shared.module";
+import { HeadersModule } from "./shared/component/headers/headers.module";
+import { CheckoutModule } from "./features/pages/checkout/checkout.module";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+    ],
+    imports: [
+        BrowserModule,
+        ProductsModule,
+        CartModule,
+        FormsModule,
+        HttpClientModule,
+        BrowserAnimationsModule,
+        SharedModule,
+        AuthenticationModule,
+        RouterOutlet,
+        AppRoutingModule,
+        HeadersModule,
+        CheckoutModule
+    ],
+    providers: [],
+    exports: [
+    ],
+    bootstrap: [AppComponent] // list on the components that should be known too angular to check and render
 })
 export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,23 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
+//import { provideRouter } from '@angular/router';
+//import routeConfig from './app/routes';
+//import {bootstrapApplication, provideProtractorTestingSupport} from "@angular/platform-browser";
+//import {AppComponent} from "./app/app.component";
+
+
+/*
+bootstrapApplication(AppModule,
+    {
+        providers: [
+            provideProtractorTestingSupport(),
+            provideRouter(routeConfig)
+        ]
+    }
+).catch(err => console.error(err));
+*/
+
+
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
